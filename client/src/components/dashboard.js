5c5bd9b1aada2dd712dbd9555259e6e6585f398c
manya2026bhasin <manyabhasin2026@gmail.com>
2024-10-28 13:04:07 +0530

    add calorie log

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,9 +1,25 @@
 import React from "react";
+import {jwtDecode} from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import CalorieLog from "./calorieLog";
 
 function Dashboard(){
+    const navigate = useNavigate();
+    function getEmailFromToken() {
+        const token = localStorage.getItem('token');
+        if (!token) return null;
+    
+        try {
+          const decodedToken = jwtDecode(token);
+          return decodedToken.email; // Assuming the email is in the payload
+        } catch (error) {
+          console.error('Failed to decode token:', error);
+          return null;
+        }
+      }
     return(
         <div>
-            this is dashboard
+            <CalorieLog getEmailFromToken={getEmailFromToken}/>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import pg from "pg";
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+import fs from 'fs';
 dotenv.config();
 
 const app = express();
@@ -20,6 +21,31 @@ const pool = new pg.Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
+});
+
+// Load food data
+const foodData = JSON.parse(fs.readFileSync('foodData.json'));
+
+app.post('/calorie-log', (req, res) => {
+    const { food, quantity } = req.body;
+    const foodItem = foodData[0].Sheet1.find(item => item.Food.toLowerCase() === food.toLowerCase());
+
+    if (foodItem) {
+        const totalCalories = (foodItem.Calories * quantity)/foodItem.Serving;
+        res.json({ food, quantity, totalCalories });
+    } else {
+        res.status(404).json({ error: "Food item not found" });
+    }
+});
+
+app.post('/calorie-log-similar-foods', (req, res) => {
+    const { food, quantity } = req.body;
+    const foodItem = foodData[0].Sheet1.filter(item => item.Food.toLowerCase().includes(food.toLowerCase()));
+    if (foodItem) {
+        res.json({ foodItem });
+    } else {
+        res.status(404).json({ error: "Food item not found" });
+    }
 });
 
 // Sign up 
