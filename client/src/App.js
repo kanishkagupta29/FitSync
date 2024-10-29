e1b25747aee663e62a5d5078ada57660fcba0b0e
John Doe <john.doe@contoso.com>
2024-10-29 11:52:18 +0530

    make some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard';
 import Home from './components/home';
 import SignUp from './components/signup';
 import LogIn from './components/login';
-<<<<<<< HEAD
 import DailyGoal from './components/daily_goal';
 import MealPlan from './components/meal_plan';
 import Workout from './components/workout';
 import ProgressTracker from './components/progress_tracker';
 import CalorieLog from './components/calorie_log';
-
-=======
 import Personalinfo from './components/personalinfo' ;
->>>>>>> 6788883a965946a13ffcbadcb826b2f400dd0748
+
 function App() {
   return (
     <Router>
--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Sidebar from "./sidebar";
 import { Outlet } from "react-router-dom";
 import "../styles/dashboard.css";
@@ -15,29 +14,6 @@ function Dashboard() {
             <div className="dashboard-content">
                 <Outlet/>
             </div>
-=======
-import {jwtDecode} from "jwt-decode";
-import { useNavigate } from "react-router-dom";
-import CalorieLog from "./calorieLog";
-
-function Dashboard(){
-    const navigate = useNavigate();
-    function getEmailFromToken() {
-        const token = localStorage.getItem('token');
-        if (!token) return null;
-    
-        try {
-          const decodedToken = jwtDecode(token);
-          return decodedToken.email; // Assuming the email is in the payload
-        } catch (error) {
-          console.error('Failed to decode token:', error);
-          return null;
-        }
-      }
-    return(
-        <div>
-            <CalorieLog getEmailFromToken={getEmailFromToken}/>
->>>>>>> 6788883a965946a13ffcbadcb826b2f400dd0748
         </div>
     );
 }
