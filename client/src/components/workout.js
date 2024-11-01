a118f971beda23b2174dc675e6754818e8748124
John Doe <john.doe@contoso.com>
2024-11-01 20:38:52 +0530

    workout added

--- client/src/components/workout.js
+++ client/src/components/workout.js
@@ -5,9 +5,12 @@ import exerciseData from "../data/exercise.json";
 function Workout() {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(1);
   const [isPlaying, setIsPlaying] = useState(true);
-  const [timer, setTimer] = useState(45);
+  const [timer, setTimer] = useState(5);
+  const [isComplete, setIsComplete] = useState(false);
 
   useEffect(() => {
+    if(isComplete) return ;
+
     if (timer > 0) {
       const interval = setInterval(() => {
         setTimer((prev) => prev - 1);
@@ -15,14 +18,21 @@ function Workout() {
       return () => clearInterval(interval);
     } else {
       if (isPlaying) {
-        setTimer(15);
+        setTimer(2);
       } else {
-        setTimer(45);
+        setTimer(5);
         setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % exerciseData.length);
       }
       setIsPlaying(!isPlaying);
     }
-  }, [timer, isPlaying]);
+  }, [timer, isPlaying, currentVideoIndex, isComplete]);
+
+  const handleRepeat = () => {
+    setCurrentVideoIndex(1); 
+    setIsPlaying(true);
+    setTimer(10);
+    setIsComplete(false); 
+  };
 
   const currentExercise = exerciseData[currentVideoIndex];
 
@@ -32,14 +42,11 @@ function Workout() {
       <h3>You are stronger than you think. Keep going!</h3>
       {isPlaying ? (
         <div className="card">
-          <iframe
-            width="420"
-            height="315"
-            src={`https://www.youtube.com/embed/${currentExercise.videos.split("v=")[1]}?autoplay=1`}
-            title={currentExercise.Excercises}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          <img
+            src={currentExercise.giphyURL !== "NULL" ? currentExercise.giphyURL : "path/to/placeholder.png"}
+            alt={currentExercise.Excercises}
+            className="exercise-animation"
+          />
           <div className="card-body">
             <h5 className="card-title">{currentExercise.Excercises.toUpperCase()}</h5>
             <p className="card-text"><b>Calories Burned:</b> {currentExercise['calories burned']}</p>
@@ -54,6 +61,12 @@ function Workout() {
             <p className="timer">Time: {timer}s</p>
           </div>
         </div>
+      )}
+
+      {isComplete && (
+        <button onClick={handleRepeat} className="repeat-button">
+          Repeat Exercises
+        </button>
       )}
     </div>
   );
