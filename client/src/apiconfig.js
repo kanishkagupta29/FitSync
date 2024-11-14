1958b37ef14e803b58acb3443e0da4cf6b0d498a
kanishkagupta29 <122289785+kanishkagupta29@users.noreply.github.com>
2024-11-14 23:48:47 +0530

    Update apiconfig.js

--- client/src/apiconfig.js
+++ client/src/apiconfig.js
@@ -1,5 +1,5 @@
 // apiConfig.js
-// const API_BASE_URL = 'https://fitsync-u0nt.onrender.com';
+const API_BASE_URL = 'https://fitsync-u0nt.onrender.com';
 // const API_BASE_URL= 'http://localhost:5000';
-const API_BASE_URL= 'https://fitsync-jktz.onrender.com';
+
 export default API_BASE_URL;
